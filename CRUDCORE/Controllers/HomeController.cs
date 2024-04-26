04ac8cd1d239a15270410923e61f9d097d30d02e
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-04-26 18:15:54 -0500

    Finalización del CRUD

--- CRUDCORE/Controllers/HomeController.cs
+++ CRUDCORE/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace CRUDCORE.Controllers
         }
 
         [HttpGet]
-        public IActionResult Empleado_Detalle()
+        public IActionResult Empleado_Detalle(int idEmpleado)
         {
             EmpleadoVM oEmpleadoVM = new EmpleadoVM()
             {
@@ -39,6 +39,12 @@ namespace CRUDCORE.Controllers
                     Value = cargo.IdCargo.ToString()
                 }).ToList()
             };
+
+            if (idEmpleado != 0)
+            {
+                oEmpleadoVM.oEmpleado = _DBContext.Empleados.Find(idEmpleado);
+            }
+
             return View(oEmpleadoVM);
         }
 
@@ -49,6 +55,27 @@ namespace CRUDCORE.Controllers
             {
                 _DBContext.Empleados.Add(oEmpleadoVM.oEmpleado);
             }
+            else
+            {
+                _DBContext.Empleados.Update(oEmpleadoVM.oEmpleado);
+            }
+
+            _DBContext.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int idEmpleado)
+        {
+            Empleado oEmpleado = _DBContext.Empleados.Include(c => c.oCargo).Where(e => e.IdEmpleado == idEmpleado).FirstOrDefault();
+            return View(oEmpleado);
+        }
+
+        [HttpPost]
+        public IActionResult Eliminar(Empleado oEmpleado)
+        {
+            _DBContext.Empleados.Remove(oEmpleado);
             _DBContext.SaveChanges();
 
             return RedirectToAction("Index", "Home");
